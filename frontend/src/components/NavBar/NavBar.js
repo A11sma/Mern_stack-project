2147b3e9d70b078517a8a874ea164ca5cfc9369e
A11sma <103256134+A11sma@users.noreply.github.com>
2022-07-07 19:45:38 +0530

    Add files via upload

---
+++ frontend/src/components/NavBar/NavBar.js
@@ -0,0 +1,31 @@
+import { Navbar, NavbarBrand, Nav, NavItem } from "react-bootstrap";
+import GitHubButton from "react-github-btn";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+          <NavbarBrand href="/" className="font-weight-bold">CV Generator App</NavbarBrand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <NavItem className="px-lg-3">
+                
+              </NavItem>
+              <NavItem className="px-lg-3">
+               
+              </NavItem>
+              <NavItem className="px-lg-3">
+               
+              
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </header>
+    </nav>
+  );
+};
+
+export default NavBar;
